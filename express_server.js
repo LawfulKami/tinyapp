51fc30103a1442bbcea00ff0b5d23052ee559096
Lanny Rivest <lanny.rivest@gmail.com>
2020-08-31 15:39:55 -0400

    Reset and start of the project

--- express_server.js
+++ express_server.js
@@ -2,18 +2,19 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.render("pages/index");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
