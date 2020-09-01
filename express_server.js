f66936f7e30bd44c3e930f69966a12f7c0c49591
Lanny Rivest <lanny.rivest@gmail.com>
2020-09-01 16:12:52 -0400

    Login Logout implemented

--- express_server.js
+++ express_server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+const cookieParser = require('cookie-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(cookieParser());
 
 const generateRandomString = () => {
   let result = '';
@@ -16,45 +18,68 @@ const generateRandomString = () => {
 };
 
 
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    urls: urlDatabase,
+    username: req.cookies.username
+  };
   res.render("urls_index", templateVars);
 });
 
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { username: req.cookies.username };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
+  let templateVars = {
+    username: req.cookies.username,
+    urls: urlDatabase,
+  };
   const longURL = urlDatabase[req.params.shortURL];
   const shortURL = req.params.shortURL;
   if (Object.keys(urlDatabase).includes(shortURL)) {
     res.redirect(longURL);
   } else {
-    res.redirect("urls_index", { urls: urlDatabase });
+    res.redirect("urls_index", templateVars);
   }
 });
 
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies.username
+  };
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls", (req, res) => {
+  let templateVars = { username: req.cookies.username };
   const id = generateRandomString();
   if (req.body.longURL.match(/^(https:\/\/|http:\/\/)/)) {
     urlDatabase[id] = req.body.longURL;
   } else {
     urlDatabase[id] = "http://" + req.body.longURL;
   }
-  res.redirect(`/urls/${id}`);
+  res.redirect(`/urls/${id}`, templateVars);
+});
+
+app.post("/login", (req, res) =>{
+  const username = req.body.username;
+  res.cookie("username", username).redirect("/urls");
+});
+
+app.post("/logout", (req, res) =>{
+  res.clearCookie("username").redirect("/urls");
 });
 
 app.post("/urls/:shortURL", (req, res) => {
